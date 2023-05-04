9fb1057332c194a5bd4df9740a3789a347bc3711
kota <52403688+yashikota@users.noreply.github.com>
2023-05-04 18:15:50 +0900

    hidden navbar

--- src/App.tsx
+++ src/App.tsx
@@ -75,7 +75,7 @@ export default function App() {
             </Grid>
 
             {/* ナビゲーション */}
-            {isSmallScreen ? <Navigation /> : <></>}
+            {/* {isSmallScreen ? <Navigation /> : <></>} */}
 
         </>
     );
